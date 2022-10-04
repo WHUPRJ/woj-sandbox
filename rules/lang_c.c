32aa186d87777f669eeee4b0bb83180bcc0b4d2b
Paul Pan <panyuxuan@hotmail.com>
2022-10-04 14:30:11 +0800

    chore: comment for syscall nr

--- rules/lang_c.c
+++ rules/lang_c.c
@@ -6,9 +6,20 @@
 
 void setup_lang_c(scmp_filter_ctx ctx) {
     int white[] = {
-        SCMP_SYS(read),  SCMP_SYS(write),      SCMP_SYS(lseek),
-        SCMP_SYS(mmap),  SCMP_SYS(munmap),     SCMP_SYS(getpid),
-        SCMP_SYS(futex), SCMP_SYS(newfstatat), SCMP_SYS(exit_group),
+        SCMP_SYS(read),            // 0
+        SCMP_SYS(write),           // 1
+        SCMP_SYS(fstat),           // 5
+        SCMP_SYS(lseek),           // 8
+        SCMP_SYS(mmap),            // 9
+        SCMP_SYS(munmap),          // 11
+        SCMP_SYS(pread64),         // 17
+        SCMP_SYS(getpid),          // 39
+        SCMP_SYS(futex),           // 202
+        SCMP_SYS(newfstatat),      // 262
+        SCMP_SYS(clock_gettime),   // 228
+        SCMP_SYS(clock_getres),    // 229
+        SCMP_SYS(clock_nanosleep), // 230
+        SCMP_SYS(exit_group),      // 231
     };
     int white_len = sizeof(white) / sizeof(white[0]);
 
