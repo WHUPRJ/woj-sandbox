b66a52f4df176ecde9cd3413e4dda6292a1823b6
Paul <panyuxuan@hotmail.com>
2022-10-09 15:43:25 +0800

    chore: more checks

--- build_libseccomp.sh
+++ build_libseccomp.sh
@@ -6,6 +6,6 @@ set -x
 git clone https://github.com/seccomp/libseccomp.git &>/dev/null
 cd libseccomp || exit 1
 git checkout $VERSION &>/dev/null
-./autogen.sh &>/dev/null
-./configure --enable-shared=no &>/dev/null
-make -j &>/dev/null
+./autogen.sh &>/dev/null || exit 1
+./configure --enable-shared=no &>/dev/null || exit 1
+make -j &>/dev/null || exit 1
